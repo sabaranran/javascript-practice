1bd0612f7d37da22a53f8f8743b23dd494ad5180
EC2 Default User <ec2-user@ip-172-31-32-228.ap-northeast-1.compute.internal>
2023-10-31 00:47:45 +0000

    2章 【JavaScriptの基本文法】

---
+++ chapter2/app.js
@@ -0,0 +1,45 @@
+/*let hello = "Hello, World";
+alert(hello);
+
+alert(4+3);
+alert(8-5);
+alert(2*6);
+alert(10/2);
+
+alert('Hello'+'World')
+
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1+str2);
+
+let orange = 100;
+let apple = 120;
+
+if(orange<apple){
+  alert('みかんの値段がりんごより安い');
+}else if(orange==apple){
+  alert('みかんとりんごが同じ値段');
+}else{
+  alert('みかんの値段がりんごより高い');
+}
+let max = 100;
+let num = 1;
+let count = 0;
+while(num<max){
+  num = num*2;
+  count = count+1;
+}
+alert('2を掛け続けて'+max+'を超えるのに必要だった回数は'+count+'回です');
+let i;
+let num = 0;
+for(i=1;i<11;i++){
+  num = num+i;
+}
+alert('1から10まで足し算した結果は'+num+'です');
+*/
+
+/*確認問題*/
+alert(100+50);
+alert(200-10);
+alert(50*20);
+alert(10000/2);
