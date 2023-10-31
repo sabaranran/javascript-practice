d323cb951384cd57446f10d5770153e292b55dde
EC2 Default User <ec2-user@ip-172-31-32-228.ap-northeast-1.compute.internal>
2023-10-31 13:53:53 +0000

    6章 【イベントを検出しよう】

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -20,8 +20,6 @@ $(function(){
 $(function(){
   $('.box1').hide(1000);
 });
-*/
-
 
 $(function(){
   $('.box1').slideDown(1000,function(){
@@ -30,6 +28,35 @@ $(function(){
       'height': '100px',
       'width': '200px'
     }).slideUp(1000);
+  });
+});
+
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color': '#0000FF'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color': '#FF0000'});
+  });
+});
+
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+});
+
+*/
+
+$(function(){
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
 
