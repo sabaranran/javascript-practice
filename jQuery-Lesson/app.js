ed675572dd8d1f26e975af74e9fcad94a890518f
EC2 Default User <ec2-user@ip-172-31-32-228.ap-northeast-1.compute.internal>
2023-10-31 12:31:58 +0000

    5章【jQueryでHTMLのCSS要素を操作しよう】

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,3 +1,35 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
   $('body').html('<h1>Hello jQuery!!</h1>');
 });
+
+$(function(){
+  $('.box1').css({
+    'background-color': '#0000FF',
+    'height': '100px'
+  });
+});
+
+$(function(){
+  $('.box1').slideDown(1000);
+});
+
+$(function(){
+  $('.box1').slideUp(10000);
+})
+
+$(function(){
+  $('.box1').hide(1000);
+});
+*/
+
+
+$(function(){
+  $('.box1').slideDown(1000,function(){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'height': '100px',
+      'width': '200px'
+    }).slideUp(1000);
+  });
+});
+
