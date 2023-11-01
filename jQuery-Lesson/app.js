ad3af5c1b372f64b24570abf6599ab9c0cafb268
EC2 Default User <ec2-user@ip-172-31-32-228.ap-northeast-1.compute.internal>
2023-11-01 02:28:10 +0000

    7章 【thisとchildren】

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -49,8 +49,6 @@ $(function(){
   });
 });
 
-*/
-
 $(function(){
   $('.box1').on('click',function(){
     $('.box1').addClass('box1-ext');
@@ -59,4 +57,35 @@ $(function(){
     $('.box1').removeClass('box1-ext');
   });
 });
+
+$(function(){
+  $('.bg1').on('click',function(){
+    $('.bg1').slideUp();
+  });
+  $('.bg2').on('click',function(){
+    $('.bg2').slideUp();
+  });
+  $('.bg3').on('click',function(){
+    $('.bg3').slideUp();
+  });
+  $('.bg4').on('click',function(){
+    $('.bg4').slideUp();
+  });
+});
+
+$(function(){
+  $('.box1').on('click',function(){
+    $(this).slideUp();
+  });
+});
+
+*/
+
+$(function(){
+  $('button').on('click',function(){
+    $('ul').children().css('color','red');
+  });
+});
+
+
 
